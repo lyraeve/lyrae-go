63d3c805e03ac615b623a6c7c37a1e7151474275
MilesChou <jangconan@gmail.com>
2019-03-10 00:38:50 +0800

    Use goquery and remove htmlquery

--- command/find.go
+++ command/find.go
@@ -2,22 +2,33 @@ package command
 
 import (
 	"fmt"
-	"github.com/antchfx/htmlquery"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/urfave/cli"
 	"gopkg.in/h2non/gentleman.v2"
+	"log"
 	"strings"
 )
 
 var (
-	Sites = map[string]string {
+	Sites = map[string]string{
 		"JavBus": "https://www.javbus.com",
+	}
+
+	RemoveString = map[string]string{
+		"number":     "識別碼:",
+		"publish_at": "發行日期:",
+		"length":     "長度:",
+		"director":   "導演:",
+		"producer":   "製作商:",
+		"publisher":  "發行商:",
+		"series":     "系列:",
 	}
 )
 
 var (
 	FindCommand = cli.Command{
-		Name:  "find",
-		Usage: "查詢",
+		Name:      "find",
+		Usage:     "查詢",
 		ArgsUsage: "<番號>",
 		Action: func(c *cli.Context) error {
 			sn := c.Args().Get(0)
@@ -39,18 +50,46 @@ var (
 				return nil
 			}
 
-			htmlNode, err := htmlquery.Parse(strings.NewReader(res.String()))
+			// Load the HTML document
+			doc, err := goquery.NewDocumentFromReader(strings.NewReader(res.String()))
 
 			if err != nil {
+				log.Fatal(err)
+
 				return err
 			}
 
-			list := htmlquery.Find(htmlNode, "//span[@class='header']/text()")
+			data := map[string]string{}
 
-			for _, node := range list {
-				fmt.Println(node.Data)
+			doc.Find(".movie .info p").Each(func(i int, s *goquery.Selection) {
+				text := s.Text()
+
+				for header, replaceString := range RemoveString {
+					if strings.Contains(text, replaceString) {
+						text = strings.TrimSpace(strings.Replace(text, replaceString, "", 1))
+						data[header] = text
+					}
+				}
+			})
+
+			for header, content := range data {
+				fmt.Println(header + ": " + content)
 			}
 
+			var actors []string
+			var categories []string
+
+			doc.Find(".movie .info p .genre").Each(func(i int, s *goquery.Selection) {
+				attr, _ := s.Attr("onmouseover")
+				if strings.Compare("", attr) == 0 {
+					actors = append(actors, strings.TrimSpace(s.Text()))
+				} else {
+					categories = append(categories, strings.TrimSpace(s.Text()))
+				}
+			})
+
+			fmt.Println(actors)
+			fmt.Println(categories)
 
 			return nil
 		},
--- go.mod
+++ go.mod
@@ -1,11 +1,9 @@
 module github.com/lyraeve/lyrae-go
 
 require (
-	github.com/antchfx/htmlquery v1.0.0
-	github.com/antchfx/xpath v0.0.0-20190129040759-c8489ed3251e // indirect
-	github.com/h2non/gentleman v2.0.3+incompatible // indirect
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
 	github.com/urfave/cli v1.20.0
 	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95 // indirect
-	golang.org/x/text v0.3.0 // indirect
 	gopkg.in/h2non/gentleman.v2 v2.0.3
 )
